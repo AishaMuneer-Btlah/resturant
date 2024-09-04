b7795a1b2b8c2ee24e2705ae23d67e3e94b66c5c
aisha <aishamuneer64@gmail.com>
2024-09-04 06:54:52 +0300

    chang in mc-donalds.dart

--- lib/mc_donalds.dart
+++ lib/mc_donalds.dart
@@ -12,14 +12,36 @@ class McDonalds extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
         Container(
-        height: 50,width: 100,
+        height: 150,width: 400,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/mac-meal.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-        )],),),
+        ),
+          Container(
+            padding: EdgeInsets.only(top: 15),
+            child: Column(children: [
+              ListTile(
+                leading: Image.asset('assets/images/mac.jpg'),
+                title: Text('Mc Donalds',style: TextStyle(fontWeight: FontWeight.bold),),
+                subtitle: Text('American cuisine,\n fast food',style: TextStyle(fontSize: 12),),
+                trailing: Icon(Icons.favorite,color: Colors.orange,),
+              ),
+              Row(children: [
+
+              ],),
+              Row(children: [
+
+              ],),
+              Row(children: [
+
+              ],)
+
+            ],),
+          )
+          ],),),
     );
   }
 }
