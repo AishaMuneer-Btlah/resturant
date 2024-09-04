23b8e4039fa54c5e64bbcf6d2cab350c99b25bb6
aisha <aishamuneer64@gmail.com>
2024-09-04 07:19:57 +0300

    add picturs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resturant/dominos.dart';
 import 'package:resturant/home_page.dart';
 import 'package:resturant/mc_donalds.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(useMaterial3: false),
       debugShowCheckedModeBanner: false,
-      home: McDonalds(),
+      home: Dominos(),
     );
   }
 }
